22ae8f1ea382dcb7c9916e63eafffe2fe3cbd363
Wallace Luk <wallace@Wallaces-MacBook-Air.local>
2016-10-17 18:25:47 -0700

    Add functional server-side file

--- queryRito.js
+++ queryRito.js
@@ -1,26 +1,16 @@
-//Consider: Turning this file into a server with express -- 
-//Maybe use npm request node to interface with RIOT API instead and
-//then do maintaining live dataset here. Client side can then
-//do a get request to here to get properly maintained dataset
-//var request = require('request');
+//Add express here later
+var settings = require('./config.js');
+//var rp = require('request-promise');
+var request = require('request');
 
 
-var getMockData = function(callback) {
-  $.ajax({
-    url: 'https://' + REGION + '.api.pvp.net/observer-mode/rest/featured',
-    type: 'GET',
-    data: {key: window.RIOT_API_KEY},
-    dataType: 'json',
-    success: function (matches) {
-      console.log(matches);
-    },
-    error: function (matches, anythingElse) {
-      if(matches) {
-        console.error(matches);
-      }
-      console.error('Error!', matches, anythingElse);
-    }
-  });
-}
-
-getMockData();
+console.log('Testing this request module!');
+request('https://' + settings.REGION + '.api.pvp.net/championmastery/location/NA1/player/'
+            + 30370360 + '/topchampions?api_key='
+        + settings.RIOT_API_KEY, function(error, res, body) {
+  console.log('Here is what I got back:', error, body);
+});
+// request('https://' + settings.REGION + '.api.pvp.net/observer-mode/rest/featured?api_key='
+//         + settings.RIOT_API_KEY, function(error, res, body) {
+//   console.log('Here is what I got back:', error, body);
+// });
