c37e9059fa1a297b0bbbd99bdbc488058661170a
Wallace Luk <wallace@Wallaces-MacBook-Air.local>
2016-10-17 22:00:41 -0700

    Add eslint; Refactor front-end

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,11 @@
+
+/**
+ * These rules enforce the Hack Reactor Style Guide
+ *
+ * Visit this repo for more information:
+ *   https://github.com/reactorcore/eslint-config-hackreactor
+ */
+
 module.exports = {
-  extend: 'hackreactor',
-  rules: {
-  }
+  extends: './node_modules/eslint-config-hackreactor/index.js'
 };
