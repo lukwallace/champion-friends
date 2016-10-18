f0e9e40276696590efef4e6ece2c8da8b58d8b1e
Wallace Luk <wallace@Wallaces-MacBook-Air.local>
2016-10-17 22:01:28 -0700

    lint

--- queryRito.js
+++ queryRito.js
@@ -8,15 +8,15 @@ var getData = function () {
     qs: {
       api_key: settings.RIOT_API_KEY
     },
-    json:true
+    json: true
   };
 
-  var url = {}
+  var url = {};
   url.uri = 'https://' + settings.REGION + '.api.pvp.net/observer-mode/rest/featured';
   return rp(_.extend(url, base))
   .then(function(matches) {
     console.log('Got matches');
-    var nameList = matches.gameList[0].participants.map(function(participant){
+    var nameList = matches.gameList[0].participants.map(function(participant) {
       return participant.summonerName;
     }).join(',');
 
@@ -26,24 +26,26 @@ var getData = function () {
   .then(function(participants) {
     console.log('Got participants');
     var IDs = [];
-    for(var name in participants) {
+    for (var name in participants) {
       IDs.push(participants[name].id);
     }
     var promisedMasteries = [];
     IDs.forEach(function(id, index) {
       url.uri = 'https://' + settings.REGION + '.api.pvp.net/championmastery/location/NA1/player/' + id + '/topchampions';
-      if(index % 3 === 0) { promisedMasteries.push(rp(_.extend(url, base))) };
+      //We're only getting every three b/c Rito has restrictions
+      if (index % 3 === 0) { 
+        promisedMasteries.push(rp(_.extend(url, base))); 
+      }
     });
     return Promise.all(promisedMasteries);
   })
-  .spread(function(masteries){
+  .spread(function(masteries) {
     console.log('Got masteries');
-    console.log(masteries);
     return masteries;
   })
   .catch(function(err) {
     console.error('Error', err.message);
   });
-}
+};
 
 module.exports.getData = getData;
